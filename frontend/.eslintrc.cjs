dd0aa1863ef353255b9adc44136223ac478a337d
Mehul8864 <mehul79067@gmail.com>
2026-03-05 08:46:50 +0530

    chore(eslint): update .eslintrc.cjs for React tooling

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,21 +1,34 @@
+// .eslintrc.cjs
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  env: {
+    browser: true,
+    es2020: true,
+    // enable node env if you lint server/config files as well
+    // node: true,
   },
-}
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended"
+  ],
+  // ignore built assets and other generated files
+  ignorePatterns: ["dist"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: { jsx: true }
+  },
+  settings: {
+    react: { version: "18.2" }
+  },
+  plugins: ["react", "react-hooks", "react-refresh"],
+  rules: {
+    "react/jsx-no-target-blank": "off",
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    // example: consider turning on these later
+    // "react/prop-types": "off",
+    // "react/react-in-jsx-scope": "off" // not needed with new JSX runtime
+  }
+};
